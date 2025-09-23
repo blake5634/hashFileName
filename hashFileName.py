b4d52a98a1550228fac427fd44278c796c3fc367
Blake Hannaford <blake.hannaford@gmail.com>
2025-09-23 13:40:59 -0700

    initial commit has new datetime log an lookup features

---
+++ hashFileName.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import numpy as np
+import datetime
+import sys
+import subprocess
+
+logfname = '/home/blake/hashlog.txt'
+av = sys.argv
+
+if len(av) == 1:
+    ext = ''
+elif len(av) == 3:
+    if av[1] == 'lookup':  # look up creation time of a prior hash
+         thash = av[2]
+         try:
+            res = (subprocess.check_output(['grep', thash, logfname])).decode('utf-8').strip()
+            print(res)
+         except:
+             print('[', thash, '] not found')
+    quit()
+elif len(av) == 2:
+    ext = av[1]
+else:
+    print('Usage: \n   > hashFileName.py  [extension]')
+    quit()
+al = list('ABCDEFGHIJKLMNPQRSTUV') # delete O to reduce confusion w 0.
+nu = list('0123456789')
+
+letters = ''.join(np.random.choice(al, 2))
+digits  = ''.join(np.random.choice(nu, 5))
+result = letters+digits+ext
+hashonly = letters+digits
+
+# log the new hash with its creation date
+ofp = open(logfname, 'a')
+#  '2011-11-03 18:21:26'
+timestr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print(timestr, ', ', hashonly, file=ofp)
+ofp.close()
+
+print('Random file hash: ', result)
